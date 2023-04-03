6d987e7ae7c60a53c847f50ee2d928278efab29a
mohd zubair ansari <mohdzubairshafi@gmail.com>
2023-04-03 05:57:42 +0530

    Add questions and question counter

--- app.js
+++ app.js
@@ -1,251 +1,238 @@
-const startquiz = document.getElementById('startquiz');
-const main = document.querySelector('main');
-const section = document.createElement('section');
-const startcard = document.getElementById('startcard');
-const span = document.getElementById('timer')
+const startquiz = document.getElementById("startquiz");
+const main = document.querySelector("main");
+const section = document.createElement("section");
+const startcard = document.getElementById("startcard");
+const span = document.getElementById("timer");
 
 let quesNo = 0;
-let score = 0
-let count = 50;
+let score = 0;
+let count = 140;
+let reward = 6.25;
 
 function start_Test() {
-    startcard.remove();
-    section.setAttribute("id", "question-section");
-    main.appendChild(section);
+  startcard.remove();
+  section.setAttribute("id", "question-section");
+  main.appendChild(section);
 }
 
-
 function get_Question() {
-    if (quesNo < questions.length) {
-        const h2 = document.createElement('h2');
-        h2.innerHTML = questions[quesNo].questionText;
-        section.appendChild(h2);
+  if (quesNo < questions.length) {
+    const h2 = document.createElement("h2");
+    h2.innerHTML = questions[quesNo].questionText;
+    section.appendChild(h2);
 
-        const options = questions[quesNo].options;
-        let text = '';
-        options.forEach((opt) => {
-            const option = document.createElement('button');
-            option.setAttribute("class", "options");
-            option.innerHTML = opt;
-            option.onclick = (e) => {
-                text = e.target.innerHTML;
-                checkanswer();
-            };
-            section.appendChild(option);
+    const options = questions[quesNo].options;
+    let text = "";
+    options.forEach((opt) => {
+      const option = document.createElement("button");
+      option.setAttribute("class", "options");
+      option.innerHTML = opt;
+      option.onclick = (e) => {
+        text = e.target.innerHTML;
+        checkanswer();
+      };
+      section.appendChild(option);
+    });
+    quesNo++;
 
-        });
-        quesNo++;
+    function checkanswer() {
+      if (quesNo != questions.length) {
+        const answer = questions[quesNo - 1].answer;
+        const currentQuestionNumber = quesNo;
+        const totalQuestions = questions.length;
+        const questionCount = document.createElement("b");
+        questionCount.innerHTML = `Question ${currentQuestionNumber} of ${totalQuestions}`;
 
-        function checkanswer() {
+        section.innerHTML = "";
+        const div = document.createElement("div");
+        const b = document.createElement("b");
+        const hr = document.createElement("hr");
+        div.classList.add("flex");
+        hr.classList.add("hr");
+        if (text === answer) {
+          score += reward;
+          b.innerHTML = " Correct ";
+        } else {
+          b.innerHTML = " Incorrect ";
+          count -= 10;
+        }
+        get_Question();
+        section.appendChild(hr);
+        div.appendChild(b);
+        div.appendChild(questionCount);
+        section.appendChild(div);
+      } else {
+        const answer = questions[quesNo - 1].answer;
 
-            if (quesNo != questions.length) {
-                const answer = questions[quesNo - 1].answer;
-
-
-                section.innerHTML = '';
-                const b = document.createElement('b');
-                const hr = document.createElement('hr');
-                hr.classList.add('hr');
-                if (text === answer) {
-                    score += 5;
-                    b.innerHTML = " Correct ";
-                } else {
-                    b.innerHTML = " Incorrect ";
-                    count -= 10;
-                }
-                get_Question();
-                section.appendChild(hr);
-                section.appendChild(b);
-            } else {
-                const answer = questions[quesNo - 1].answer;
-
-                if (text === answer) {
-                    score += 5;
-                }
-
-                section.innerHTML = '';
-                get_score();
-
-            }
+        if (text === answer) {
+          score += 5;
         }
 
+        section.innerHTML = "";
+        get_score();
+      }
     }
-
-
+  }
 }
 
 function get_score() {
+  const h2 = document.createElement("h2");
+  h2.innerHTML = "ALL DONE !!!";
 
-    const h2 = document.createElement('h2');
-    h2.innerHTML = "ALL DONE !!!";
+  section.appendChild(h2);
+  section.setAttribute("id", "scoreboard");
+  const span = document.createElement("span");
 
-    section.appendChild(h2);
-    section.setAttribute("id", "scoreboard");
-    const span = document.createElement('span');
+  const div = document.createElement("div");
+  const p = document.createElement("p");
 
-    const div = document.createElement('div');
-    const p = document.createElement('p');
+  p.innerHTML = `your final score is ${score}/${reward * questions.length} `;
 
-    p.innerHTML = "your final score is " + score;
+  span.innerHTML = "Enter initials: ";
 
-    span.innerHTML = "Enter initials: ";
+  const input = document.createElement("input");
+  const form = document.createElement("form");
+  form.setAttribute("onsubmit", "return false");
+  input.setAttribute("required", "");
 
-    const input = document.createElement('input');
-    const form = document.createElement('form');
-    form.setAttribute("onsubmit", "return false")
-    input.setAttribute("required", "");
+  input.placeholder = "Your Name";
+  const button = document.createElement("input");
+  button.setAttribute("type", "submit");
+  button.id = "submit";
+  button.classList.add("options");
+  button.innerHTML = "Submit";
+  section.appendChild(p);
+  div.appendChild(span);
+  form.appendChild(input);
+  form.appendChild(button);
+  div.appendChild(form);
+  section.appendChild(div);
 
-    input.placeholder = "Your Name";
-    const button = document.createElement('input');
-    button.setAttribute("type", "submit");
-    button.id = "submit";
-    button.classList.add('options')
-    button.innerHTML = "Submit";
-    section.appendChild(p);
-    div.appendChild(span);
-    form.appendChild(input)
-    form.appendChild(button);
-    div.appendChild(form);
-    section.appendChild(div);
-
-    form.onsubmit = getname;
-
-
+  form.onsubmit = getname;
 }
-
-
-
 
 let playerscore = new Array();
 
-
 function getname() {
+  const input = document.querySelector("input");
+  const name = input.value;
+  const yourScore = score;
 
-    const input = document.querySelector('input');
-    const name = input.value;
-    const yourScore = score;
+  playerscore = JSON.parse(localStorage.getItem("players")) ? JSON.parse(localStorage.getItem("players")) : [];
 
-    playerscore = JSON.parse(localStorage.getItem("players")) ? JSON.parse(localStorage.getItem("players")) : [];
+  let newplayer = {
+    name: name,
+    score: yourScore,
+  };
 
-    let newplayer = {
-        "name": name,
-        "score": yourScore
-    };
+  if (
+    playerscore.some((o) => {
+      return o.name == newplayer.name;
+    })
+  ) {
+    playerscore.forEach((element, index) => {
+      if (element.score < newplayer.score && element.name == newplayer.name) {
+        playerscore[index].score = newplayer.score;
+      }
+    });
+  } else {
+    playerscore.push(newplayer);
+  }
 
-    if (playerscore.some((o) => { return o.name == newplayer.name; })) {
+  localStorage.setItem("players", JSON.stringify(playerscore));
 
-        playerscore.forEach((element, index) => {
-
-            if (element.score < newplayer.score && element.name == newplayer.name) {
-                playerscore[index].score = newplayer.score;
-            }
-        });
-    } else {
-        playerscore.push(newplayer);
-    }
-
-    localStorage.setItem("players", JSON.stringify(playerscore));
-
-    highscore();
-};
-
-
+  highscore();
+}
 
 function highscore() {
+  section.innerHTML = "";
+  const h2 = document.createElement("h2");
+  h2.innerHTML = "HIGH SCORES";
+  h2.style.textAlign = "center";
+  section.setAttribute("id", "highscore");
 
-    section.innerHTML = '';
-    const h2 = document.createElement('h2');
-    h2.innerHTML = "HIGH SCORES";
-    h2.style.textAlign = 'center';
-    section.setAttribute("id", "highscore");
+  const topPlayer = JSON.parse(localStorage.getItem("players")) ? JSON.parse(localStorage.getItem("players")) : [];
 
+  const div = document.createElement("div");
+  const pdiv = document.createElement("div");
 
+  pdiv.classList.add("pdiv");
+  div.classList.add("flex");
 
-    const topPlayer = JSON.parse(localStorage.getItem("players")) ? JSON.parse(localStorage.getItem("players")) : [];
+  const go_back = document.createElement("button");
+  const clear_highscore = document.createElement("button");
 
+  go_back.classList.add("options");
+  clear_highscore.classList.add("options");
 
-    const div = document.createElement('div');
-    const pdiv = document.createElement('div');
+  go_back.onclick = returnhome;
+  clear_highscore.onclick = deleteScore;
 
-    pdiv.classList.add('pdiv');
-    div.classList.add('flex');
+  go_back.innerHTML = "Go Back";
+  clear_highscore.innerHTML = "Clear High Score";
+  section.appendChild(h2);
 
-    const go_back = document.createElement('button');
-    const clear_highscore = document.createElement('button');
+  if (topPlayer.length === 0) {
+    const p = document.createElement("p");
+    p.innerHTML = ` NO HIGH SCORE YET `;
+    pdiv.appendChild(p);
+  } else {
+    topPlayer.sort((a, b) => (a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1));
+    topPlayer.sort((a, b) => {
+      return b.score - a.score;
+    });
+    topPlayer.forEach((player, index) => {
+      const p = document.createElement("p");
+      p.innerHTML = `${index + 1}.  ${player.name} - ${player.score}`;
 
-    go_back.classList.add('options');
-    clear_highscore.classList.add('options');
-
-    go_back.onclick = returnhome;
-    clear_highscore.onclick = deleteScore;
-
-
-    go_back.innerHTML = "Go Back";
-    clear_highscore.innerHTML = "Clear High Score";
-    section.appendChild(h2);
-
-    if (topPlayer.length === 0) {
-
-        const p = document.createElement('p');
-        p.innerHTML = ` NO HIGH SCORE YET `;
-        pdiv.appendChild(p);
-    } else {
-        topPlayer.sort((a, b) => a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1)
-        topPlayer.sort((a, b) => { return b.score - a.score })
-        topPlayer.forEach((player, index) => {
-            const p = document.createElement('p');
-            p.innerHTML = `${index + 1}.  ${player.name} - ${player.score}`;
-
-            pdiv.appendChild(p);
-        });
-    }
-    div.appendChild(go_back);
-    div.appendChild(clear_highscore);
-    section.appendChild(pdiv);
-    section.appendChild(div);
-    main.appendChild(section);
+      pdiv.appendChild(p);
+    });
+  }
+  div.appendChild(go_back);
+  div.appendChild(clear_highscore);
+  section.appendChild(pdiv);
+  section.appendChild(div);
+  main.appendChild(section);
 }
 
 function returnhome() {
-    location.reload();
+  location.reload();
 }
 
 function deleteScore() {
-    localStorage.removeItem("players");
-    highscore();
+  localStorage.removeItem("players");
+  highscore();
 }
 
-const leaderboard = document.getElementById('leaderboard');
+const leaderboard = document.getElementById("leaderboard");
 leaderboard.onclick = () => {
-    startcard.remove();
-    highscore();
+  startcard.remove();
+  highscore();
 };
 
-startquiz.addEventListener('click', start_Test);
-startquiz.addEventListener('click', get_Question);
-startquiz.addEventListener('click', countdown);
-
+startquiz.addEventListener("click", start_Test);
+startquiz.addEventListener("click", get_Question);
+startquiz.addEventListener("click", countdown);
 
 function countdown() {
-    let counter = setInterval(timer, 1000);
+  let counter = setInterval(timer, 1000);
 
-    function timer() {
-        count--;
-        if (section.id === 'scoreboard') {
-            clearInterval(counter);
-            span.innerHTML = 0;
+  function timer() {
+    count--;
+    if (section.id === "scoreboard") {
+      clearInterval(counter);
+      span.innerHTML = 0;
 
-            section.innerHTML = '';
-            get_score();
-        } else if (count >= 0) {
-            span.innerHTML = count;
-        } else if (count < 0) {
-            clearInterval(counter);
-            span.innerHTML = 0;
+      section.innerHTML = "";
+      get_score();
+    } else if (count >= 0) {
+      span.innerHTML = count;
+    } else if (count < 0) {
+      clearInterval(counter);
+      span.innerHTML = 0;
 
-            section.innerHTML = '';
-            get_score();
-        }
-
+      section.innerHTML = "";
+      get_score();
     }
+  }
 }
--- assets/script.js
+++ assets/script.js
@@ -1,36 +1,112 @@
-const questions = [{
-        questionText: "Commonly used data types DO NOT include:",
-        options: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
-        answer: "3. alerts",
-    },
-    {
-        questionText: "Arrays in JavaScript can be used to store ______.",
-        options: [
-            "1. numbers and strings",
-            "2. other arrays",
-            "3. booleans",
-            "4. all of the above",
-        ],
-        answer: "4. all of the above",
-    },
-    {
-        questionText: "String values must be enclosed within _____ when being assigned to variables.",
-        options: ["1. commas", "2. curly brackets", "3. quotes", "4. parentheses"],
-        answer: "3. quotes",
-    },
-    {
-        questionText: "A very useful tool used during development and debugging for printing content to the debugger is:",
-        options: [
-            "1. JavaScript",
-            "2. terminal/bash",
-            "3. for loops",
-            "4. console.log",
-        ],
-        answer: "4. console.log",
-    },
-    {
-        questionText: "Which of the following is a statement that can be used to terminate a loop, switch or label statement?",
-        options: ["1. break", "2. stop", "3. halt", "4. exit"],
-        answer: "1. break",
-    },
+const questions = [
+  {
+    questionText: "Commonly used data types DO NOT include:",
+    options: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+    answer: "3. alerts",
+  },
+  {
+    questionText: "Arrays in JavaScript can be used to store ______.",
+    options: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
+    answer: "4. all of the above",
+  },
+  {
+    questionText: "String values must be enclosed within _____ when being assigned to variables.",
+    options: ["1. commas", "2. curly brackets", "3. quotes", "4. parentheses"],
+    answer: "3. quotes",
+  },
+  {
+    questionText: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    options: ["1. JavaScript", "2. terminal/bash", "3. for loops", "4. console.log"],
+    answer: "4. console.log",
+  },
+  {
+    questionText:
+      "Which of the following is a statement that can be used to terminate a loop, switch or label statement?",
+    options: ["1. break", "2. stop", "3. halt", "4. exit"],
+    answer: "1. break",
+  },
+  {
+    questionText: "What is the output of the following code? \nconsole.log(Math.round(2.5))",
+    options: ["1. 2", "2. 3", "3. 2.5", "4. 3.5"],
+    answer: "2. 3",
+  },
+  {
+    questionText: "What is the difference between let and var in JavaScript?",
+    options: [
+      "2. let can be re declared, but var cannot",
+      "3. var has function scope, but let has block scope",
+      "4. let has function scope, but var has block scope",
+    ],
+    answer: "3. var has function scope, but let has block scope",
+  },
+  {
+    questionText: "What is a closure in JavaScript?",
+    options: [
+      "1. A function that has access to variables in its parent scope",
+      "2. A function that returns another function",
+      "3. A function that has no name",
+      "4. A function that has a default parameter",
+    ],
+    answer: "1. A function that has access to variables in its parent scope",
+  },
+  {
+    questionText: "What is a promise in JavaScript?",
+    options: [
+      "1. An object that represents a value that may not be available yet, but will be resolved at some point in the future",
+      "2. A way to iterate over a collection of elements",
+      "3. A method that sorts an array in place",
+      "4. A function that creates a new object",
+    ],
+    answer:
+      "1. An object that represents a value that may not be available yet, but will be resolved at some point in the future",
+  },
+  {
+    questionText: "What does the async keyword do in JavaScript?",
+    options: [
+      "1. It makes a function run synchronously",
+      "2. It makes a function return a promise",
+      "3. It makes a function run in a separate thread",
+      "4. It has no effect on a function",
+    ],
+    answer: "2. It makes a function return a promise",
+  },
+  {
+    questionText: "What is the result of the following code? \n[1, 2, 3].map(x => x * 2)",
+    options: ["1. [2, 4, 6]", "2. [1, 4, 9]", "3. [2, 3, 6]", "4. [1, 2, 3]"],
+    answer: "1. [2, 4, 6]",
+  },
+  {
+    questionText:
+      "What is the result of the following code? \nlet obj = {a: 1, b: 2, c: 3}; \nlet {a, b, c} = obj; \nconsole.log(a, b, c);",
+    options: ["1. 1, 2, 3", "2. undefined, undefined, undefined", "3. {a: 1, b: 2, c: 3}", "4. ReferenceError"],
+    answer: "1. 1, 2, 3",
+  },
+  {
+    questionText:
+      "Which of the following methods returns a new array containing all elements of the original array that pass a test?",
+    options: ["1. filter()", "2. reduce()", "3. map()", "4. sort()"],
+    answer: "1. filter()",
+  },
+  {
+    questionText:
+      "Which of the following methods returns a new array with the results of calling a function for every array element?",
+    options: ["1. forEach()", "2. reduce()", "3. map()", "4. filter()"],
+    answer: "3. map()",
+  },
+  {
+    questionText:
+      "What is the output of the following code? \nvar x = 5;\n(function() {\n  console.log(x);\n  var x = 10;\n})();",
+    options: ["1. 5", "2. 10", "3. undefined", "4. ReferenceError"],
+    answer: "3. undefined",
+  },
+  {
+    questionText: "What does the Promise.all() method do?",
+    options: [
+      "1. Resolves multiple promises in parallel",
+      "2. Resolves multiple promises in series",
+      "3. Rejects multiple promises in parallel",
+      "4. Rejects multiple promises in series",
+    ],
+    answer: "1. Resolves multiple promises in parallel",
+  },
 ];
